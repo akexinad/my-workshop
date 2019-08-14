278e97c95a1707dfc843696cea8bd5711fb1163c
akexinad <danixeka@gmail.com>
2019-08-14 20:29:26 +1000

    my first console log with c sharp

--- c_sharp/ConsoleApp1/ConsoleApp1/Program.cs
+++ c_sharp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,21 +4,36 @@ namespace ConsoleApp1
 {
     public class BankAccount
     {
+        // A constructor is a member that has the same name as the class.
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+        }
+
         // Properties are data elements and can have code that enforces validation or other rules.
+        public string Number { get; }
         public string Owner { get; set; }
         public decimal Balance { get; }
 
 
         // Methods are blocks f code that perform a single function.
         // Reading the names of each of the members should provide enough information for you or another developer to understand what the cleral amount, DateTime date, string note)
+        public void MakeDeposit(decimal amount, DateTime date, string note)
         {
         }
 
         public void MakeWithdrawal(decimal amount, DateTime date, string note)
         {
         }
+    }
 
-
-
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("fellini", 4000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with an initial balance of {account.Balance}.");
+        }
     }
 }
