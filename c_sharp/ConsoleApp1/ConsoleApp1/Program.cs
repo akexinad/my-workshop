af2d4d238df1bd680403e8e46ada879fcbd24457
akexinad <danixeka@gmail.com>
2019-08-14 20:54:22 +1000

    getting the hang of it

--- c_sharp/ConsoleApp1/ConsoleApp1/Program.cs
+++ c_sharp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,13 +9,16 @@ namespace ConsoleApp1
         {
             this.Owner = name;
             this.Balance = initialBalance;
+
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
         }
 
         // Properties are data elements and can have code that enforces validation or other rules.
         public string Number { get; }
         public string Owner { get; set; }
         public decimal Balance { get; }
-
+        private static int accountNumberSeed = 20190001;
 
         // Methods are blocks f code that perform a single function.
         // Reading the names of each of the members should provide enough information for you or another developer to understand what the cleral amount, DateTime date, string note)
@@ -26,6 +29,11 @@ namespace ConsoleApp1
         public void MakeWithdrawal(decimal amount, DateTime date, string note)
         {
         }
+
+        public void Description()
+        {
+            Console.WriteLine($"Account {Number} was created for {Owner} with an initial balance of {Balance}.");
+        }
     }
 
     class Program
@@ -33,7 +41,11 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             var account = new BankAccount("fellini", 4000);
-            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with an initial balance of {account.Balance}.");
+            account.Description();
+            // Console.WriteLine($"Account {Number} was created for {account.Owner} with an initial balance of {account.Balance}.");
+
+            var accountTwo = new BankAccount("Benigni", 8000);
+            accountTwo.Description();
         }
     }
 }
