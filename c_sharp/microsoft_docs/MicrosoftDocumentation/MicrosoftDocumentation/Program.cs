9aa6fafea78cd14db37c3925b0bf9be39a65c6c4
danny <danixeka@gmail.com>
2019-08-16 08:08:07 +1000

    microsoft docs

---
+++ c_sharp/microsoft_docs/MicrosoftDocumentation/MicrosoftDocumentation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MicrosoftDocumentation
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacciNumber = new List<int> { 1, 1 };
+
+
+            while (fibonacciNumber.Count <= 20)
+            {
+                var previous = fibonacciNumber[fibonacciNumber.Count - 1];
+                var previous2 = fibonacciNumber[fibonacciNumber.Count - 2];
+                fibonacciNumber.Add(previous + previous2);
+            }
+
+            foreach (var num in fibonacciNumber)
+            {
+                Console.WriteLine(num);
+            }
+
+            
+        }
+    }
+}
