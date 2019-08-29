c2406572b8c3073f3b57780f96c6f2fb9786c11a
danny <danny.ceccattini@lendlease.com>
2019-08-29 10:31:48 +1000

    code format

--- threejs/raycaster_ex/src/raycaster.js
+++ threejs/raycaster_ex/src/raycaster.js
@@ -2,95 +2,89 @@ let selection;
 let selectedStructure;
 const selectedColour = 0xffff00;
 
-
 function createRaycaster() {
+    raycaster.setFromCamera(mouse, camera);
 
-    raycaster.setFromCamera( mouse, camera );
+    selection = raycaster.intersectObjects(scene.children);
 
-    selection = raycaster.intersectObjects( scene.children );
-    
     if (selection.length > 0) {
-        
-        if ( selectedStructure !=- selection[ 0 ].object ) {
-
+        if (selectedStructure != -selection[0].object) {
             if (selectedStructure) {
-                selectedStructure.forEach( compartment => {
-                    compartment.material.emissive.setHex( selectedStructure.originalColour );
+                selectedStructure.forEach(compartment => {
+                    compartment.material.emissive.setHex(
+                        selectedStructure.originalColour
+                    );
                 });
             }
-            
-            selectedStructure = scene.children.filter( compartments => {
+
+            selectedStructure = scene.children.filter(compartments => {
                 return compartments.name === selection[0].object.name;
             });
-    
+
             selectedStructure.originalColour = selectedStructure[0].material.emissive.getHex();
 
-            selectedStructure.forEach( compartment => {
-                compartment.material.emissive.setHex( selectedColour );
+            selectedStructure.forEach(compartment => {
+                compartment.material.emissive.setHex(selectedColour);
             });
         }
-        
     } // else {
-        
-        // if (selectedStructure) {
-        //     selectedStructure.forEach(compartment => {
-        //         compartment.material.emissive.setHex( selectedStructure.originalColour );
-        //     });
-        // }
 
-        // selectedStructure = null;
+    // if (selectedStructure) {
+    //     selectedStructure.forEach(compartment => {
+    //         compartment.material.emissive.setHex( selectedStructure.originalColour );
+    //     });
     // }
-    
+
+    // selectedStructure = null;
+    // }
 }
 
 function createButton(text, callback) {
-
-    const output = document.querySelector( '#output' );
-    const btn = document.createElement( 'button' );
+    const output = document.body;
+    const btn = document.createElement("button");
 
     btn.innerText = text;
-    output.appendChild( btn );
+    output.appendChild(btn);
 
-    btn.addEventListener( 'click', callback );
+    btn.addEventListener("click", callback);
 }
 
-const addCompartmentBtn = createButton( 'Add Compartment', addCompartment );
+const addCompartmentBtn = createButton("Add Compartment", addCompartment);
 
-function addCompartment() { 
+function addCompartment() {
+    msg("Selected structure ====>", selectedStructure);
 
-    msg('Selected structure ====>', selectedStructure)
-
-    
     let topCompartment;
     let newCompartment;
-    
+
     if (!selectedStructure) {
-        return msg( 'Pick a tower!' );
+        return msg("Pick a tower!");
     }
 
-    
-    topCompartment = selectedStructure[ 0 ];
+    topCompartment = selectedStructure[0];
 
     const highlightedColor = topCompartment.material.emissive.getHex();
-    
+
     newCompartment = new OliveClone(topCompartment);
-    newCompartment.material.emissive.setHex( highlightedColor );
-    
-    msg('TOP COMPARTMENT =====>', topCompartment);
-    msg('ADDED COMPARTMENT ====>', newCompartment);
+    newCompartment.material.emissive.setHex(highlightedColor);
+
+    msg("TOP COMPARTMENT =====>", topCompartment);
+    msg("ADDED COMPARTMENT ====>", newCompartment);
 
     selectedStructure.unshift(newCompartment);
-    msg('TOWER AFTER ADDITION ====>', selectedStructure);
+    msg("TOWER AFTER ADDITION ====>", selectedStructure);
 
     scene.add(newCompartment);
 
-    msg('selectedStructure after adding it to the scene ====>', selectedStructure);
+    msg(
+        "selectedStructure after adding it to the scene ====>",
+        selectedStructure
+    );
 }
-
 
 function render() {
     createRaycaster();
-    renderer.render( scene, camera );
+    renderer.render(scene, camera);
 }
 
 function animate() {
