6bc147a73675637c62d0416f40fdd4a85e73601e
danny <danny.ceccattini@lendlease.com>
2019-09-05 16:04:39 +1000

    generating lines with threebox

--- threebox/js/main.js
+++ threebox/js/main.js
@@ -23,10 +23,42 @@ var map = new mapboxgl.Map({
     bearing: 180
 });
 
+
+//////////////////////////////////////////////////////////////////
+
+// randomly generate some line arcs (not essential for understanding this demo)
+
+const lines = new Array();
+const arcSegments = 25;
+const lineQuantity = 50;
+for (const i = 0; i < lineQuantity; i++){
+    const line = new Array();
+    const destination = [300*(Math.random()-0.5), 140*(Math.random()-0.5)];
+    const maxElevation = Math.pow(Math.abs(destination[0]*destination[1]), 0.5) * 80000;
+    const increment = destination.map(function(direction){
+        return direction/arcSegments;
+    })
+    for (const l = 0; l<=arcSegments; l++){
+        const waypoint = increment.map(function(direction){
+            return direction * l
+        })
+        const waypointElevation = Math.sin(Math.PI*l/arcSegments) * maxElevation;
+        waypoint.push(waypointElevation);
+        line.push(waypoint);
+    }
+    lines.push(line)
+}
+console.log('lineGeometries of the lines: ', lines);
+
+
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
 function addMarker() {
 
     // create a HTML element for marker
-    var el = document.createElement('div');
+    const el = document.createElement('div');
     el.className = 'marker';
 
     // instantiate the marker and add it to the map 
@@ -73,6 +105,21 @@ function displayModels() {
 
             displayProject(tb, eustonProjectData, coords.euston);
             displayProject(tb, silvertownProjectData, coords.silvertown, false);
+
+
+            // RENDERING LINES
+
+            for (line of lines) {
+                const lineOptions = {
+                    geometry: line,
+                    color: (line[1][1]/180) * 0xffffff, // color based on latitude of endpoint
+                    width: Math.random() + 1 // random width between 1 and 2
+                }
+                lineMesh = tb.line(lineOptions);
+                tb.add(lineMesh)
+            }
+            
+            
         },
         
         render() {
@@ -102,3 +149,5 @@ document.getElementById('2d')
 
 document.getElementById('3d')
     .addEventListener('click', display3d);
+
+// DRAWING A LINE
