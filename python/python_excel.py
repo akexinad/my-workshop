2249e785c9243f37e141d727d56eb0edd1e0c60b
danny <danny.ceccattini@lendlease.com>
2019-10-15 11:22:45 +1100

    cannot write to cells via python due to permissions

--- python/python_excel.py
+++ python/python_excel.py
@@ -1,3 +1,18 @@
 import openpyxl
+import os
 
-openpyxl.__version__
+# print(openpyxl.__version__)
+
+filename = 'pyxl.xlsx'
+os.chdir('c:\\Users\\cecd304\\me\\documents')
+wb = openpyxl.load_workbook(filename)
+
+# print(type(wb))
+
+sheet1 = wb['Sheet1']
+
+sheet1['d4'].value = 'HELLO WORLD'
+
+wb.save(filename)
+
+# print(sheet1['b1:b7'])
