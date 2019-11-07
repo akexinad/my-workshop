5cee8e395a50e377784e38cc2ff21bfc7936042c
akexinad <danny.ceccattini@lendlease.com>
2019-11-07 13:54:58 +1100

    update

--- apollo/start/client/src/pages/login.js
+++ apollo/start/client/src/pages/login.js
@@ -1,5 +1,35 @@
 import React from 'react';
+import { useApolloClient, useMutation } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+import { LoginForm, Loading } from "../components";
+
+const LOGIN_USER = gql`
+  mutation login($email: String!) {
+    login(email: $email)
+  }
+`;
 
 export default function Login() {
-  return <div />;
+  const client = useApolloClient();
+  const [login, { loading, error }] = useMutation(
+    LOGIN_USER,
+    {
+      onCompleted({ login }) {
+        console.log(login);
+        localStorage.setItem('token', login);
+        client.writeData({ data: { isLoggedIn: true }})
+      }
+    }
+  );
+
+  if (loading) {
+    return <Loading />
+  }
+
+  if (error) {
+    return <p>THERE WAS ERROR: { error.message }</p>
+  }
+  
+  return <LoginForm login={ login } />;
 }
