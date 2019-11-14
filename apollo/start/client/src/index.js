17fe9adcdbb4208545515e0fdaf71eb584fb33b8
Danny <danny.ceccattini@lendlease.com>
2019-11-14 14:05:35 +1100

    connecting

---
+++ apollo/start/client/src/index.js
@@ -0,0 +1,40 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import gql from "graphql-tag";
+
+import Pages from "./pages";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:4000/'
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+
+client
+  .query({
+    query: gql`
+      query GetLaunch {
+        launch(id: 56) {
+          id
+          mission {
+            name
+          }
+        }
+      }
+    `
+  })
+  .then(res => console.log(res));
+
+ReactDOM.render(
+  <ApolloProvider client={ client }>
+    <Pages />
+  </ApolloProvider>, document.getElementById('root')
+);
