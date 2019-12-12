cc53b7e264f4d8582dbf48a5ad16bb3f1360efb1
Danny <danny.ceccattini@lendlease.com>
2019-12-12 16:07:02 +1100

    pyodide

--- pyodide/pyodide_react/src/App.jsx
+++ pyodide/pyodide_react/src/App.jsx
@@ -1,17 +1,19 @@
-import React, { useEffect } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+/*global languagePluginLoader */
+/*global pyodide */
 
 const App = () => {
-
-  useEffect( () => {
+  useEffect(() => {
     const script = document.createElement("script");
 
     script.src = "https://pyodide.cdn.iodide.io/pyodide.js";
     script.async = true;
-    script.onload = () =>  scriptLoaded();
-    
-    console.log('script has loaded');
-     
+    script.onload = () => scriptLoaded();
+
+    console.log("script has loaded");
+
     document.body.appendChild(script);
 
     return () => {
@@ -19,18 +21,47 @@ const App = () => {
     };
   }, []);
 
-  const scriptLoaded = () => {
+  const [code, setCode] = useState("");
+  const [result, setResult] = useState(null);
+  const [isPyodideReady, setPyStatus] = useState(false);
+
+  function scriptLoaded() {
+    console.log("pyodide not run yet");
     languagePluginLoader.then(() => {
-      
-    })
+      console.log("pyodide starting");
+      // pyodide is now ready to use...
+      console.log(pyodide.runPython("import sys\nsys.version"));
+      pyodide.runPython("import sys\nsys.version");
+      pyodide.loadPackage(["matplotlib", "numpy"]);
+      setPyStatus(true)
+    });
   }
 
-  
+  const execute = (code) => {
+    return pyodide.runPython(code);
+  } 
+
   return (
     <div className="App">
       <h1>Py</h1>
+      <textarea
+        name="code"
+        value={code}
+        cols="60"
+        rows="20"
+        onChange={e => setCode(e.target.value)}
+        disabled={ !isPyodideReady }
+      />
+      <button
+        onClick={() => setResult( execute(code) )}
+        disabled={ !isPyodideReady }
+      >
+        RUN
+      </button>
+      <h3>RESULT</h3>
+      <p>{ result }</p>
     </div>
   );
-}
+};
 
 export default App;
