3c7f60d7434db53a6b400b5bffb9984ecbbbbba8
Danny <danny.ceccattini@lendlease.com>
2019-12-16 12:07:03 +1100

    rendering

--- threebox/react_threebox/src/components/Mapbox/Mapbox.tsx
+++ threebox/react_threebox/src/components/Mapbox/Mapbox.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useRef } from "react";
+import React, { FC, useState, useRef, useEffect, Fragment } from "react";
 import mapboxgl from "mapbox-gl";
 // @ts-ignore
 import { Threebox } from "threebox-map";
@@ -9,51 +9,56 @@ import { layer3dBuidlings } from "../../utils/mapboxLayers/layer3dBuildings";
 
 import "mapbox-gl/dist/mapbox-gl.css";
 
-mapboxgl.accessToken = TOKENS.MAPBOX;
-
-const Mapbox: React.FC = () => {
-  const mapRef = useRef(mapboxgl);
-  const mapbox = mapRef.current;
-
-  let map: mapboxgl.Map;
-
-  useEffect(() => {
-    map = new mapRef.current.Map({
-      container: "map",
-      style: "mapbox://styles/mapbox/dark-v10",
-      center: COORDINATES.EUSTON,
-      zoom: 15,
-      pitch: 60,
-      bearing: 0
-    });
-
-    return () => {
-      map.remove();
-    };
-  }, []);
-
-  const addMapboxLayer = () => {
-    map.addLayer(layer3dBuidlings);
-  };
-
-  const removeMapboxLayer = () => {
-    map.removeLayer(layer3dBuidlings.id);
-  };
-
-  const style = {
+const styles = {
     width: "100vw",
     height: "100vh",
     margin: 0
-  };
+};
 
-  return (
-    <Fragment>
-      <h2>Mapbox Component</h2>
-      <button onClick={addMapboxLayer}>ADD LAYER</button>
-      <button onClick={removeMapboxLayer}>REMOVE LAYER</button>
-      <div style={style} id="map"></div>
-    </Fragment>
-  );
+const Mapbox: FC = () => {
+    const [map, setMap] = useState<mapboxgl.Map>(null);
+    const mapContainer = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        mapboxgl.accessToken = TOKENS.MAPBOX;
+
+        const initializeMap = ({ setMap, mapContainer }: any) => {
+            const map = new mapboxgl.Map({
+                container: mapContainer.current,
+                style: "mapbox://styles/mapbox/dark-v10",
+                center: COORDINATES.EUSTON,
+                zoom: 15,
+                pitch: 60,
+                bearing: 0
+            });
+
+            map.on("load", () => {
+                setMap(map);
+                map.resize();
+            });
+        };
+
+        if (!map) {
+            initializeMap({ setMap, mapContainer });
+        }
+    }, [map]);
+
+    const addMapboxLayer = () => {
+        map.addLayer(layer3dBuidlings);
+    };
+
+    const removeMapboxLayer = () => {
+        map.removeLayer(layer3dBuidlings.id);
+    };
+
+    return (
+        <Fragment>
+            <h2>Mapbox Component</h2>
+            <button onClick={addMapboxLayer}>ADD LAYER</button>
+            <button onClick={removeMapboxLayer}>REMOVE LAYER</button>
+            <div style={styles} ref={el => (mapContainer.current = el)}></div>
+        </Fragment>
+    );
 };
 
 export default Mapbox;
