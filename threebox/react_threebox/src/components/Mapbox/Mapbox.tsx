ff18f6583dc3b1d0ce2973894b9269575fb50f0c
akexinad <danixeka@gmail.com>
2019-12-17 18:40:04 +1100

    issue reading commits

--- threebox/react_threebox/src/components/Mapbox/Mapbox.tsx
+++ threebox/react_threebox/src/components/Mapbox/Mapbox.tsx
@@ -8,10 +8,8 @@ import TOKENS from "../../utils/tokens";
 import { mapLayer3dBuidlings } from "../../utils/mapBoxUtils/mapboxLayers/mapLayer3dBuildings";
 import { mapLayerThreebox } from "../../utils/mapBoxUtils/mapboxLayers/mapLayerthreebox";
 import "mapbox-gl/dist/mapbox-gl.css";
-import { GeoJSONify } from "../../utils/mapBoxUtils/geoJSONify";
 
 // import { EUSTON_DATA_191210 } from "../../data/191210_eustonData";
-import { DATA_GEOJSON_ZONING_SANTA_CLARA } from "../../data/geoJSONZoningSantaClara";
 
 declare global {
     interface Window {
@@ -77,17 +75,9 @@ const Mapbox: FC = () => {
         mapLayerThreebox(map);
     };
 
-    const addZoningLayer = () => {
-        const geoJSON = new GeoJSONify(DATA_GEOJSON_ZONING_SANTA_CLARA);
-        geoJSON.layerListByBasezone.forEach(item => {
-            map.addLayer(item.layer);
-        });
-    };
-
     return (
         <Fragment>
             <h2>Mapbox Component</h2>
-            <button onClick={addZoningLayer}>ADD ZONING DATA</button>
             <button onClick={toggleMapboxLayer}>
                 {mapLayer ? "REMOVE LAYER" : "ADD LAYER"}
             </button>
