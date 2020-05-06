3a5c0252ebbc0873a993a1a31efb3d60b6409a33
danny <danixeka@gmail.com>
2020-05-06 18:35:51 +1000

    decreased size of floor to prepare to decrease sky scale

--- mock_react_apps/threeact/src/components/Floor/Floor.tsx
+++ mock_react_apps/threeact/src/components/Floor/Floor.tsx
@@ -11,7 +11,7 @@ const Floor: FC<ReactThreeFiber.Object3DNode<Mesh, typeof Mesh>> = (props) => {
 
     return (
         <mesh {...props} ref={floor} receiveShadow={true} castShadow={true}>
-            <planeGeometry attach="geometry" args={[1000, 1000, 1, 1]} />
+            <planeGeometry attach="geometry" args={[500, 500, 1, 1]} />
             <meshPhongMaterial attach="material" color="green" />
         </mesh>
     );
