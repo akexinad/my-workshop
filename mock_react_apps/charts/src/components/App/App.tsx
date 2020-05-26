41f0760ba674374a552eb434ecb831d4b5cf5d81
danny <danixeka@gmail.com>
2020-05-26 18:00:16 +1000

    changes to app tsx

--- mock_react_apps/charts/src/components/App/App.tsx
+++ mock_react_apps/charts/src/components/App/App.tsx
@@ -1,27 +1,33 @@
 import React, { FC } from "react";
 import "./App.css";
 // import TreemapChart from "../TreemapChart/TreemapChart";
-import data, { data2, data3 } from "../../data";
-import BarChartRecharts from "../BarChartRecharts/BarChartRecharts";
-import BarChartGoogle from "../BarChartGoogle/BarChartGoogle";
+// import data, { data2, data3 } from "../../data";
+// import BarChartRecharts from "../BarChartRecharts/BarChartRecharts";
+// import BarChartGoogle from "../BarChartGoogle/BarChartGoogle";
+// import BarChartVictory from "../BarChartVictory/BarChartVictory";
+import DonutChartVictory from "../DonutChartVictory/DonutChartVictory";
+import { mockRentalData } from "../../data/mockRentalData";
 
-const COLORS = [
-    "#8889DD",
-    "#9597E4",
-    "#8DC77B",
-    "#A5D297",
-    "#E2CF45",
-    "#d40000"
+const data = [
+    { type: "Studio", amount: "50000000" },
+    { type: "1 Bedroom", amount: "25000000" },
+    { type: "2 Bedrooms", amount: "25000000" }
 ];
-
-const COLORS2 = ["#d40000", "#00ff00", "#000000", "#0000ff", "#ff9200"];
 
 const App: FC = () => {
     return (
         <div className="App">
             <header className="App-header">
                 <h2>Charts</h2>
-                <BarChartRecharts
+                {/* <BarChartVictory /> */}
+                <DonutChartVictory
+                    isGrouped={false}
+                    data={mockRentalData}
+                    chartWidth={600}
+                    x={"name"}
+                    y={"amount"}
+                />
+                {/* <BarChartRecharts
                     data={data3}
                     width={1000}
                     height={400}
@@ -29,8 +35,8 @@ const App: FC = () => {
                     barKey={"pv"}
                     barSize={30}
                     colors={["#d40000"]}
-                />
-                <BarChartGoogle />
+                /> */}
+                {/* <BarChartGoogle /> */}
                 {/* <TreemapChart data={data2} colors={COLORS2} /> */}
             </header>
         </div>
