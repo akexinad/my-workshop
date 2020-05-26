a7d32895481b72ea2417f1079636140fecc1c113
danny <danixeka@gmail.com>
2020-05-26 17:59:44 +1000

    added new colour set

---
+++ mock_react_apps/charts/src/constants.ts
@@ -0,0 +1,15 @@
+export const COLORS = ["#ff0000", "#00ff00", "#0000ff", "#d40000"];
+
+export const COLORS2 = ["#d40000", "#00ff00", "#000000", "#0000ff", "#ff9200"];
+
+export const ELI_COLORS = [
+    "#fce809", // yellow
+    "#cbeb8a", // green
+    "#7bd2db", // blue
+    "#fef8b4", // light yellow
+    "#e5f5c4", // light green
+    "#def6fc", // light blue
+    "#a8de3c", // dark green
+    "#23b4c3", // dark blue
+    "#0a7d89", // navy
+];
