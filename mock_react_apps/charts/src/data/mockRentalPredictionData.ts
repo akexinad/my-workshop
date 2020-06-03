1b71dca2461c210a0be0c7dd29f91c96e2d98746
danny <danixeka@gmail.com>
2020-06-03 17:22:11 +1000

    created mock rental prediction data

---
+++ mock_react_apps/charts/src/data/mockRentalPredictionData.ts
@@ -0,0 +1,70 @@
+export const mockRentalPredictionData = [
+    {
+        type: "Studio",
+        predictions: [
+            {
+                year: 2020,
+                value: 10000
+            },
+            {
+                year: 2021,
+                value: 20000
+            },
+            {
+                year: 2022,
+                value: 30000
+            }
+        ]
+    },
+    {
+        type: "1 Bedroom",
+        predictions: [
+            {
+                year: 2020,
+                value: 20000
+            },
+            {
+                year: 2021,
+                value: 30000
+            },
+            {
+                year: 2022,
+                value: 40000
+            }
+        ]
+    },
+    {
+        type: "2 Bedroom",
+        predictions: [
+            {
+                year: 2020,
+                value: 30000
+            },
+            {
+                year: 2021,
+                value: 40000
+            },
+            {
+                year: 2022,
+                value: 50000
+            }
+        ]
+    },
+    {
+        type: "3 Bedroom",
+        predictions: [
+            {
+                year: 2020,
+                value: 50000
+            },
+            {
+                year: 2021,
+                value: 60000
+            },
+            {
+                year: 2022,
+                value: 70000
+            }
+        ]
+    }
+];
