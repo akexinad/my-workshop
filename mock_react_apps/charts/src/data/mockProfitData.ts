923297492c47ccdd2cc761f8ea378d9a14fca22e
danny <danixeka@gmail.com>
2020-06-04 11:32:21 +1000

    added mock data

--- mock_react_apps/charts/src/data/mockProfitData.ts
+++ mock_react_apps/charts/src/data/mockProfitData.ts
@@ -1,5 +1,9 @@
 export const mockProfitData = [
     {
+        year: 2014,
+        profit: 0
+    },
+    {
         year: 2015,
         profit: 10000000
     },
@@ -13,18 +17,42 @@ export const mockProfitData = [
     },
     {
         year: 2018,
-        profit: 25000000
-    },
-    {
-        year: 2019,
         profit: 40000000
     },
     {
+        year: 2019,
+        profit: 50000000
+    },
+    {
         year: 2020,
-        profit: 10000000
+        profit: 60000000
     },
     {
         year: 2021,
-        profit: 50000000
-    }
-]
+        profit: 70000000
+    },
+    {
+        year: 2022,
+        profit: 80000000
+    },
+    // {
+    //     year: 2023,
+    //     profit: 90000000
+    // },
+    // {
+    //     year: 2024,
+    //     profit: 100000000
+    // },
+    // {
+    //     year: 2025,
+    //     profit: 120000000
+    // },
+    // {
+    //     year: 2026,
+    //     profit: 130000000
+    // },
+    // {
+    //     year: 2027,
+    //     profit: 140000000
+    // }
+];
