7b033c6f10a6a7107705936fa2516069b9968ccd
akexinad <danixeka@gmail.com>
2020-11-13 15:05:36 +1100

    mapping columns and data for react table

--- mock_react_apps/python_tables/src/App.tsx
+++ mock_react_apps/python_tables/src/App.tsx
@@ -1,104 +1,67 @@
 import React from "react";
-import { Column, Row, UseTableInstanceProps } from "react-table";
 import "./App.css";
 import { Table } from "./components/Table/Table";
-import { makeData } from "./data/makeData";
 import { mockRegionTableData } from "./data/mockRegionTableData";
-import { PythonRow } from "./types";
+import { RegionTableRow, RegionColumn } from "./types";
 import capitalizeEach from "./utils/capitalizeEach";
 
 function App() {
-  // const columns: Array<Column> = React.useMemo(
-  //   () => [
-  //     {
-  //       Header: "Name",
-  //       Footer: "Name",
-  //       sticky: "left",
-  //       columns: [
-  //         {
-  //           Header: "First Name",
-  //           Footer: "First Name",
-  //           accessor: "firstName",
-  //         },
-  //         {
-  //           Header: "Last Name",
-  //           Footer: "Last Name",
-  //           accessor: "lastName",
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       Header: "Info",
-  //       Footer: "Info",
-  //       columns: [
-  //         {
-  //           Header: "Age",
-  //           Footer: (info: UseTableInstanceProps<PythonRow>) => {
-  //             const total = info.rows.reduce(
-  //               (sum: number, row: Row<PythonRow>) => row.values.age + sum,
-  //               0
-  //             );
-  //             const average = Math.round(total / info.rows.length);
-  //             return <div>Moyenne: {average}</div>;
-  //           },
-  //           accessor: "age",
-  //           width: 50,
-  //         },
-  //         {
-  //           Header: "Visits",
-  //           Footer: "Visits",
-  //           accessor: "visits",
-  //           width: 60,
-  //         },
-  //         {
-  //           Header: "Status",
-  //           Footer: "Status",
-  //           accessor: "status",
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       Header: " ",
-  //       Footer: " ",
-  //       sticky: "right",
-  //       columns: [
-  //         {
-  //           Header: "Profile Progress",
-  //           Footer: "Profile Progress",
-  //           accessor: "progress",
-  //         },
-  //       ],
-  //     },
-  //   ],
-  //   []
-  // );
+  const FROZEN_COLUMNS = 5;
 
-  // const data = React.useMemo(() => makeData(40), []);
+  const columns: Array<RegionColumn> = mockRegionTableData.headers.map(
+    (header, index) => {
+      // Make the first set of columns freeze on the left
+      while (index < FROZEN_COLUMNS) {
+        return {
+          Header: capitalizeEach(header),
+          accessor: header,
+          Footer: capitalizeEach(mockRegionTableData.footers[index]),
+          sticky: "left",
+        };
+      }
 
-  // console.log("data", data);
-
-  const frozenColumns = 5;
-
-  const columnHeaders: Array<Column> = mockRegionTableData.header.map((item, index) => {
-    const columns: Array<Column> = []
-    
-    let frozenColumnCount = 0;
-
-    while (frozenColumnCount < frozenColumns) {
-      columns.push({
-        Header: capitalizeEach(item),
-        accessor: item,
-        sticky: "left",
-        Footer: capitalizeEach(mockRegionTableData.footer[index])
-      })
-
+      return {
+        Header: capitalizeEach(header),
+        accessor: header,
+        Footer: capitalizeEach(mockRegionTableData.footers[index]),
+      };
     }
+  );
+
+  /**
+   * transform the array of arrays into an array of objects,
+   * attaching the accessor property to each cell value so
+   * that it is readable for react-table.
+   */
+  const data = mockRegionTableData.rows.map((row) => {
     
-  })
+    let regionCellObject: RegionTableRow = {};
+
+    row.forEach((cell, index) => {
+      if (!columns[index]) return;
+
+      const columnAccessor = columns[index].accessor;
+
+      const value = {
+        [columnAccessor]: cell === 0 ? "-" : cell,
+      };
+
+      regionCellObject = {
+        ...regionCellObject,
+        ...value,
+      };
+    });
+
+    return regionCellObject;
+  });
+
+  const memoColumns = React.useMemo(() => columns, [columns]);
+
+  const memoData = React.useMemo(() => data, [data]);
 
   return (
     <div className="App">
-      <Table columns={columns} data={data} />
+      <Table regionColumns={memoColumns} data={memoData} />
     </div>
   );
 }
