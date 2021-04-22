4eaa33655b4e7ff0c12dce46a113827b24995996
akexinad <danixeka@gmail.com>
2021-04-22 13:48:16 +1000

    refactored restart fusion script

--- python/scripts/reset_fusion.py
+++ python/scripts/reset_fusion.py
@@ -13,10 +13,6 @@ os.makedirs(dest_main)
 os.makedirs(dest_local)
 os.makedirs(dest_roaming)
 
-os.system("cp -r " + local + "/'Web Services' " + dest_local)
-os.system("cp -r " + local + "/'Neutron Platform' " + dest_local)
-os.system("cp -r " + roaming + "/'Neutron Platform' " + dest_roaming)
-
-os.system("rm -rf " + local + "/'Web Services'")
-os.system("rm -rf " + local + "/'Neutron Platform'")
-os.system("rm -rf " + roaming + "/'Neutron Platform'")
+shutil.move(local + "/Web Services", dest_local)
+shutil.move(local + "/Neutron Platform", dest_local)
+shutil.move(roaming + "/Neutron Platform", dest_roaming)
