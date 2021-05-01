8594627477187c87b831fd7aea34d3bb01cc5cf4
akexinad <danixeka@gmail.com>
2021-05-01 18:14:14 +1000

    types

---
+++ apollo/server/src/types.ts
@@ -0,0 +1,17 @@
+export type LaunchData = {
+    flight_number: number;
+    launch_date_unix: string;
+    launch_site: {
+        site_name: string;
+    };
+    mission_name: string;
+    links: {
+        mission_patch_small: string;
+        mission_patch: string;
+    };
+    rocket: {
+        rocket_id: string;
+        rocket_name: string;
+        rocket_type: string;
+    };
+};
