cf85cb4c5e9305ad007ba86e2d69219aa2c8d247
akexinad <danixeka@gmail.com>
2021-05-01 18:15:09 +1000

    wip

---
+++ apollo/server/src/index.ts
@@ -0,0 +1,34 @@
+import { ApolloServer } from "apollo-server";
+import env from "dotenv";
+import "reflect-metadata"; // Package required for decorators to work.
+import { buildSchema } from "type-graphql";
+// import { buildSchema } from "type-graphql";
+// import { mocks } from "./data/mockData";
+import { LaunchAPI } from "./dataSources/launch";
+import { TrackResolver } from "./resolvers/track";
+
+const main = async () => {
+    env.config();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [TrackResolver],
+            validate: false
+        }),
+        // mocks: mocks,
+        context: {
+            dataSources: () => ({
+                launchAPI: new LaunchAPI()
+            })
+        }
+    });
+
+    apolloServer.listen().then(() => {
+        console.log(
+            `\nServer is running on http://localhost:${process.env.PORT}`,
+            `\nYou can also query at https://studio.apollographql.com/dev\n`
+        );
+    });
+};
+
+main();
